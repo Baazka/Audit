cc9e64838f68738978c9294d3d32b9e13b54cfa0
Batochir <baazka0705@gmail.com>
2021-05-17 16:26:13 +0800

    Revert "-stat last"
    This reverts commit ed3a1a700175b84b5ab674c4851e1a17b0d54ad4.

--- Audit/Models/BM1.cs
+++ Audit/Models/BM1.cs
@@ -19,22 +19,19 @@ namespace Audit.Models
         public int ID { get; set; }
         [Required(ErrorMessage = "BM0 сонгоно уу.")]
         public int AUDIT_ID { get; set; }
-        [Required(ErrorMessage = "Төрийн аудитын байгууллага сонгоно уу.")]
-        public int OFFICE_ID { get; set; }
         public string DEPARTMENT_NAME { get; set; }
         public int STATISTIC_PERIOD { get; set; }
         public string PERIOD_LABEL { get; set; }
-        public int AUDIT_YEAR { get; set; }
-        public int AUDIT_TYPE { get; set; }
+        public string YEAR_LABEL { get; set; }
         public string AUDIT_TYPE_NAME { get; set; }
-        public string AUDIT_CODE { get; set; }
-        public string AUDIT_NAME { get; set; }
-        public int AUDIT_BUDGET_TYPE { get; set; }
-        public string BUDGET_TYPE_NAME { get; set; }
-        [Required(ErrorMessage = "Утга оруулна уу.")]
-        public string ORDER_DATE { get; set; }
-        [Required(ErrorMessage = "Утга оруулна уу.")]
+        public string TOPIC_TYPE_NAME { get; set; }
+        public string TOPIC_CODE { get; set; }
+        public string TOPIC_NAME { get; set; }
         public string ORDER_NO { get; set; }
+        public string ORDER_DATE { get; set; }
+        public string BUDGET_TYPE_NAME { get; set; }
+
+        public string ACT_DATE { get; set; }
         public string ACT_NO { get; set; }
         public string ACT_VIOLATION_DESC { get; set; }
         public int ACT_VIOLATION_TYPE { get; set; }
@@ -49,8 +46,11 @@ namespace Audit.Models
         public string ACT_RCV_NAME { get; set; }
         public string ACT_RCV_ROLE { get; set; }
         public string ACT_RCV_GIVEN_NAME { get; set; }
+        public string ACT_RCV_PHONE { get; set; }
         public string ACT_RCV_ADDRESS { get; set; }
+        public int ACT_CONTROL_AUDITOR_ID { get; set; }
         public string ACT_CONTROL_AUDITOR { get; set; }
+        public string COMPLETION_DATE { get; set; }
         public string COMPLETION_ORDER { get; set; }
         public decimal COMPLETION_AMOUNT { get; set; }
         public decimal COMPLETION_STATE_AMOUNT { get; set; }
@@ -59,9 +59,11 @@ namespace Audit.Models
         public decimal COMPLETION_OTHER_AMOUNT { get; set; }
         public decimal REMOVED_AMOUNT { get; set; }
         public decimal REMOVED_LAW_AMOUNT { get; set; }
-        public string REMOVED_LAW_DATE_NO { get; set; }
+        public string REMOVED_LAW_DATE { get; set; }
+        public string REMOVED_LAW_NO { get; set; }
         public decimal REMOVED_INVALID_AMOUNT { get; set; }
-        public string REMOVED_INVALID_DATE_NO { get; set; }
+        public string REMOVED_INVALID_DATE { get; set; }
+        public string REMOVED_INVALID_NO { get; set; }
         public decimal ACT_C2_AMOUNT { get; set; }
         public decimal ACT_C2_NONEXPIRED { get; set; }
         public decimal ACT_C2_EXPIRED { get; set; }
@@ -72,7 +74,6 @@ namespace Audit.Models
         public int IS_ACTIVE { get; set; } = 1;
 
         public string CREATED_DATE { get; set; } = DateTime.Now.ToString("dd-MMM-yy");
-        public DateTime? UPDATED_DATE { get; set; }
         public List<Department> departments { get; set; } = new List<Department>();
         public List<Period> periods { get; set; } = new List<Period>();
         public List<REF_AUDIT_YEAR> refaudityears { get; set; } = new List<REF_AUDIT_YEAR>();
@@ -88,33 +89,31 @@ namespace Audit.Models
                     ID = Convert.ToInt32(xml.Element("ID").Value);
                 if (xml.Element("AUDIT_ID") != null)
                     AUDIT_ID = Convert.ToInt32(xml.Element("AUDIT_ID").Value);
-                if (xml.Element("OFFICE_ID") != null)
-                    OFFICE_ID = Convert.ToInt32(xml.Element("OFFICE_ID").Value);
                 if (xml.Element("DEPARTMENT_NAME") != null)
                     DEPARTMENT_NAME = xml.Element("DEPARTMENT_NAME").Value;
                 if (xml.Element("STATISTIC_PERIOD") != null)
                     STATISTIC_PERIOD = Convert.ToInt32(xml.Element("STATISTIC_PERIOD").Value);
                 if (xml.Element("PERIOD_LABEL") != null)
                     PERIOD_LABEL = xml.Element("PERIOD_LABEL").Value;
-                
-                if (xml.Element("AUDIT_YEAR") != null)
-                    AUDIT_YEAR = Convert.ToInt32(xml.Element("AUDIT_YEAR").Value);
-                if (xml.Element("AUDIT_TYPE") != null)
-                    AUDIT_TYPE = Convert.ToInt32(xml.Element("AUDIT_TYPE").Value);
+                if (xml.Element("YEAR_LABEL") != null)
+                    YEAR_LABEL = xml.Element("YEAR_LABEL").Value;
                 if (xml.Element("AUDIT_TYPE_NAME") != null)
                     AUDIT_TYPE_NAME = xml.Element("AUDIT_TYPE_NAME").Value;
-                if (xml.Element("AUDIT_CODE") != null)
-                    AUDIT_CODE = xml.Element("AUDIT_CODE").Value;
-                if (xml.Element("AUDIT_NAME") != null)
-                    AUDIT_NAME = xml.Element("AUDIT_NAME").Value;
-                if (xml.Element("AUDIT_BUDGET_TYPE") != null)
-                    AUDIT_BUDGET_TYPE = Convert.ToInt32(xml.Element("AUDIT_BUDGET_TYPE").Value);
-                if (xml.Element("BUDGET_TYPE_NAME") != null)
-                    BUDGET_TYPE_NAME = xml.Element("BUDGET_TYPE_NAME").Value;
-                if (xml.Element("ORDER_DATE") != null)
-                    ORDER_DATE = xml.Element("ORDER_DATE").Value;
+                if (xml.Element("TOPIC_TYPE_NAME") != null)
+                    TOPIC_TYPE_NAME = xml.Element("TOPIC_TYPE_NAME").Value;
+                if (xml.Element("TOPIC_CODE") != null)
+                    TOPIC_CODE = xml.Element("TOPIC_CODE").Value;
+                if (xml.Element("TOPIC_NAME") != null)
+                    TOPIC_NAME = xml.Element("TOPIC_NAME").Value;
                 if (xml.Element("ORDER_NO") != null)
                     ORDER_NO = xml.Element("ORDER_NO").Value;
+                if (xml.Element("ORDER_DATE") != null)
+                    ORDER_DATE = Convert.ToDateTime(xml.Element("ORDER_DATE").Value).ToString("yyyy.MM.dd");
+                if (xml.Element("BUDGET_TYPE_NAME") != null)
+                    BUDGET_TYPE_NAME = xml.Element("BUDGET_TYPE_NAME").Value;
+
+                if (xml.Element("ACT_DATE") != null)
+                    ACT_DATE = Convert.ToDateTime(xml.Element("ACT_DATE").Value).ToString("yyyy.MM.dd");
                 if (xml.Element("ACT_NO") != null)
                     ACT_NO = xml.Element("ACT_NO").Value;
                 if (xml.Element("ACT_VIOLATION_DESC") != null)
@@ -124,9 +123,9 @@ namespace Audit.Models
                 if (xml.Element("VIOLATION_NAME") != null)
                     VIOLATION_NAME = xml.Element("VIOLATION_NAME").Value;
                 if (xml.Element("ACT_SUBMITTED_DATE") != null)
-                    ACT_SUBMITTED_DATE = xml.Element("ACT_SUBMITTED_DATE").Value;
+                    ACT_SUBMITTED_DATE = Convert.ToDateTime(xml.Element("ACT_SUBMITTED_DATE").Value).ToString("yyyy.MM.dd");
                 if (xml.Element("ACT_DELIVERY_DATE") != null)
-                    ACT_DELIVERY_DATE = xml.Element("ACT_DELIVERY_DATE").Value;
+                    ACT_DELIVERY_DATE = Convert.ToDateTime(xml.Element("ACT_DELIVERY_DATE").Value).ToString("yyyy.MM.dd");
                 if (xml.Element("ACT_AMOUNT") != null)
                     ACT_AMOUNT = Convert.ToDecimal(xml.Element("ACT_AMOUNT").Value);
                 if (xml.Element("ACT_STATE_AMOUNT") != null)
@@ -143,10 +142,14 @@ namespace Audit.Models
                     ACT_RCV_ROLE = xml.Element("ACT_RCV_ROLE").Value;
                 if (xml.Element("ACT_RCV_GIVEN_NAME") != null)
                     ACT_RCV_GIVEN_NAME = xml.Element("ACT_RCV_GIVEN_NAME").Value;
+                if (xml.Element("ACT_RCV_PHONE") != null)
+                    ACT_RCV_PHONE = xml.Element("ACT_RCV_PHONE").Value;
                 if (xml.Element("ACT_RCV_ADDRESS") != null)
                     ACT_RCV_ADDRESS = xml.Element("ACT_RCV_ADDRESS").Value;
                 if (xml.Element("ACT_CONTROL_AUDITOR") != null)
                     ACT_CONTROL_AUDITOR = xml.Element("ACT_CONTROL_AUDITOR").Value;
+                if (xml.Element("COMPLETION_DATE") != null)
+                    COMPLETION_DATE = Convert.ToDateTime(xml.Element("COMPLETION_DATE").Value).ToString("yyyy.MM.dd");
                 if (xml.Element("COMPLETION_ORDER") != null)
                     COMPLETION_ORDER = xml.Element("COMPLETION_ORDER").Value;
                 if (xml.Element("COMPLETION_AMOUNT") != null)
@@ -163,12 +166,16 @@ namespace Audit.Models
                     REMOVED_AMOUNT = Convert.ToDecimal(xml.Element("REMOVED_AMOUNT").Value);
                 if (xml.Element("REMOVED_LAW_AMOUNT") != null)
                     REMOVED_LAW_AMOUNT = Convert.ToDecimal(xml.Element("REMOVED_LAW_AMOUNT").Value);
-                if (xml.Element("REMOVED_LAW_DATE_NO") != null)
-                    REMOVED_LAW_DATE_NO = xml.Element("REMOVED_LAW_DATE_NO").Value;
+                if (xml.Element("REMOVED_LAW_DATE") != null)
+                    REMOVED_LAW_DATE = Convert.ToDateTime(xml.Element("REMOVED_LAW_DATE").Value).ToString("yyyy.MM.dd");
+                if (xml.Element("REMOVED_LAW_NO") != null)
+                    REMOVED_LAW_NO = xml.Element("REMOVED_LAW_NO").Value;
                 if (xml.Element("REMOVED_INVALID_AMOUNT") != null)
                     REMOVED_INVALID_AMOUNT = Convert.ToDecimal(xml.Element("REMOVED_INVALID_AMOUNT").Value);
-                if (xml.Element("REMOVED_INVALID_DATE_NO") != null)
-                    REMOVED_INVALID_DATE_NO = xml.Element("REMOVED_INVALID_DATE_NO").Value;
+                if (xml.Element("REMOVED_INVALID_DATE") != null)
+                    REMOVED_INVALID_DATE = Convert.ToDateTime(xml.Element("REMOVED_INVALID_DATE").Value).ToString("yyyy.MM.dd");
+                if (xml.Element("REMOVED_INVALID_NO") != null)
+                    REMOVED_INVALID_NO = xml.Element("REMOVED_INVALID_NO").Value;
                 if (xml.Element("ACT_C2_AMOUNT") != null)
                     ACT_C2_AMOUNT = Convert.ToDecimal(xml.Element("ACT_C2_AMOUNT").Value);
                 if (xml.Element("ACT_C2_NONEXPIRED") != null)
@@ -191,20 +198,12 @@ namespace Audit.Models
             return new XElement("BM1",
                        new XElement("ID", ID),
                        new XElement("AUDIT_ID", AUDIT_ID),
-                       new XElement("OFFICE_ID", OFFICE_ID),
-                       new XElement("STATISTIC_PERIOD", STATISTIC_PERIOD),
-                       new XElement("AUDIT_YEAR", AUDIT_YEAR),
-                       new XElement("AUDIT_TYPE", AUDIT_TYPE),
-                       new XElement("AUDIT_CODE", AUDIT_CODE),
-                       new XElement("AUDIT_NAME", AUDIT_NAME),
-                       new XElement("AUDIT_BUDGET_TYPE", AUDIT_BUDGET_TYPE),
-                       new XElement("ORDER_DATE", ORDER_DATE),
-                       new XElement("ORDER_NO", ORDER_NO),
+                       new XElement("ACT_DATE", Convert.ToDateTime(ACT_DATE).ToString("dd-MMM-yy")),
                        new XElement("ACT_NO", ACT_NO),
                        new XElement("ACT_VIOLATION_DESC", ACT_VIOLATION_DESC),
                        new XElement("ACT_VIOLATION_TYPE", ACT_VIOLATION_TYPE),
-                       new XElement("ACT_SUBMITTED_DATE", ACT_SUBMITTED_DATE),
-                       new XElement("ACT_DELIVERY_DATE", ACT_DELIVERY_DATE),
+                       new XElement("ACT_SUBMITTED_DATE", Convert.ToDateTime(ACT_SUBMITTED_DATE).ToString("dd-MMM-yy")),
+                       new XElement("ACT_DELIVERY_DATE", Convert.ToDateTime(ACT_DELIVERY_DATE).ToString("dd-MMM-yy")),
                        new XElement("ACT_AMOUNT", ACT_AMOUNT),
                        new XElement("ACT_STATE_AMOUNT", ACT_STATE_AMOUNT),
                        new XElement("ACT_LOCAL_AMOUNT", ACT_LOCAL_AMOUNT),
@@ -213,8 +212,10 @@ namespace Audit.Models
                        new XElement("ACT_RCV_NAME", ACT_RCV_NAME),
                        new XElement("ACT_RCV_ROLE", ACT_RCV_ROLE),
                        new XElement("ACT_RCV_GIVEN_NAME", ACT_RCV_GIVEN_NAME),
+                       new XElement("ACT_RCV_PHONE", ACT_RCV_PHONE),
                        new XElement("ACT_RCV_ADDRESS", ACT_RCV_ADDRESS),
-                       new XElement("ACT_CONTROL_AUDITOR", ACT_CONTROL_AUDITOR),
+                       //new XElement("ACT_CONTROL_AUDITOR", ACT_CONTROL_AUDITOR),
+                       new XElement("COMPLETION_DATE", Convert.ToDateTime(COMPLETION_DATE).ToString("dd-MMM-yy")),
                        new XElement("COMPLETION_ORDER", COMPLETION_ORDER),
                        new XElement("COMPLETION_AMOUNT", COMPLETION_AMOUNT),
                        new XElement("COMPLETION_STATE_AMOUNT", COMPLETION_STATE_AMOUNT),
@@ -223,9 +224,11 @@ namespace Audit.Models
                        new XElement("COMPLETION_OTHER_AMOUNT", COMPLETION_OTHER_AMOUNT),
                        new XElement("REMOVED_AMOUNT", REMOVED_AMOUNT),
                        new XElement("REMOVED_LAW_AMOUNT", REMOVED_LAW_AMOUNT),
-                       new XElement("REMOVED_LAW_DATE_NO", REMOVED_LAW_DATE_NO),
+                       new XElement("REMOVED_LAW_DATE", Convert.ToDateTime(REMOVED_LAW_DATE).ToString("dd-MMM-yy")),
+                       new XElement("REMOVED_LAW_NO", REMOVED_LAW_NO),
                        new XElement("REMOVED_INVALID_AMOUNT", REMOVED_INVALID_AMOUNT),
-                       new XElement("REMOVED_INVALID_DATE_NO", REMOVED_INVALID_DATE_NO),
+                       new XElement("REMOVED_INVALID_DATE", Convert.ToDateTime(REMOVED_INVALID_DATE).ToString("dd-MMM-yy")),
+                       new XElement("REMOVED_INVALID_NO", REMOVED_INVALID_NO),
                        new XElement("ACT_C2_AMOUNT", ACT_C2_AMOUNT),
                        new XElement("ACT_C2_NONEXPIRED", ACT_C2_NONEXPIRED),
                        new XElement("ACT_C2_EXPIRED", ACT_C2_EXPIRED),
