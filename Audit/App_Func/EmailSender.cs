efcf38c47e108075de7895dd2cc17deefd56b0ab
Batochir <baazka0705@gmail.com>
2022-03-27 20:33:49 +0800

    -@ func

---
+++ Audit/App_Func/EmailSender.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Onimes.WebUI.AppCode
+{
+    public class EmailSettings
+    {
+        public string MailServer { get; set; }
+        public int MailPort { get; set; }
+        public string SenderName { get; set; }
+        public string Sender { get; set; }
+        public string Password { get; set; }
+    }
+    public interface IEmailSender
+    {
+        void SendEmailAsync(string email, string subject, string htmlMessage, string ccemail = null, string attachmentPath = null);
+    }
+
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailSender(EmailSettings emailSettings)
+        {
+            _emailSettings = emailSettings;
+        }
+
+        public void SendEmailAsync(string email, string subject, string message, string ccemail = null, string attachmentPath = null)
+        {
+            try
+            {
+                // Credentials
+                var credentials = new NetworkCredential(_emailSettings.Sender, _emailSettings.Password);
+
+                // Mail message
+                var mail = new MailMessage()
+                {
+                    From = new MailAddress(_emailSettings.Sender, _emailSettings.SenderName),
+                    Subject = subject,
+                    Body = message,
+                    IsBodyHtml = true
+                };
+
+                mail.To.Add(new MailAddress(email));
+                if (ccemail != null)
+                    mail.CC.Add(new MailAddress(ccemail));
+
+                if (attachmentPath != null)
+                {
+                    // Add Attachment
+                    mail.Attachments.Add(new Attachment(attachmentPath));
+                }
+
+                // Smtp client
+                var client = new SmtpClient()
+                {
+                    Port = _emailSettings.MailPort,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Host = _emailSettings.MailServer,
+                    EnableSsl = true,
+                    Credentials = credentials
+                };
+
+                // Send it...         
+                client.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                // TODO: handle exception
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+    }
+}
